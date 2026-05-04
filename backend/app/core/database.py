82d296bfba1f89049e3a556578752f55fb449ea7
PREM KUMAR R <166322609+prem-ramamoorthy@users.noreply.github.com>
2026-05-04 23:52:15 +0530

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -5,8 +5,7 @@ from sqlalchemy.pool import NullPool
 
 engine = create_async_engine(
     "postgresql+asyncpg://postgres.iclbvtwyfboirfuvsipr:premvit%40200@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres",
-    echo=False,
-    poolclass=None,
+    echo=True,
     connect_args={
         "statement_cache_size": 0
     }
